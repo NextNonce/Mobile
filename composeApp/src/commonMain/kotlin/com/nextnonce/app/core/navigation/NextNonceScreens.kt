78f59017f9e3e164e6ad88a1e4f58603be69dcc4
bombonchik <sashayuriychuk672@gmail.com>
2025-06-17 16:20:21 +0200

    Add NextNonceScreens (Start, Auth, Portfolio)

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/core/navigation/NextNonceScreens.kt
@@ -0,0 +1,12 @@
+package com.nextnonce.app.core.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Start
+
+@Serializable
+object Auth
+
+@Serializable
+object Portfolio
