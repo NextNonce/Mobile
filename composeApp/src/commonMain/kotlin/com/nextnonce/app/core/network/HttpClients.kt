c404cd6db576b483e5ae5a033555782543fc3faf
bombonchik <sashayuriychuk672@gmail.com>
2025-06-17 23:24:02 +0200

    Add http client mapping

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/core/network/HttpClients.kt
@@ -0,0 +1,6 @@
+package com.nextnonce.app.core.network
+
+import org.koin.core.qualifier.named
+
+// HTTP client for backend communicationAdd commentMore actions
+val backendHttpClient = named("BackendHttpClient")
