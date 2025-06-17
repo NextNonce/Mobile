8931763d87fb4b5837d7f260d821f4b120d802c9
bombonchik <sashayuriychuk672@gmail.com>
2025-06-17 16:30:34 +0200

    Add AuthProvider

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/core/auth/AuthProvider.kt
@@ -0,0 +1,29 @@
+package com.nextnonce.app.core.auth
+
+import com.nextnonce.app.BuildKonfig
+import com.nextnonce.app.core.platform.Platform
+import com.nextnonce.app.core.platform.currentPlatform
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.compose.auth.ComposeAuth
+import io.github.jan.supabase.compose.auth.googleNativeLogin
+import io.github.jan.supabase.createSupabaseClient
+
+object AuthProvider {
+    lateinit var client: SupabaseClient
+        private set
+
+    fun init() {
+        client = createSupabaseClient(
+            BuildKonfig.SUPABASE_URL,
+            BuildKonfig.SUPABASE_ANON_KEY
+        ) {
+            install(Auth)
+            install(ComposeAuth) {
+                if (currentPlatform is Platform.Android) {
+                    googleNativeLogin(BuildKonfig.GOOGLE_WEB_CLIENT_ID)
+                }
+            }
+        }
+    }
+}
