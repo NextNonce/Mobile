5d637bb60217d8484db26f51ae8bb5afadbc3a7f
bombonchik <sashayuriychuk672@gmail.com>
2025-06-18 00:11:21 +0200

    Properly implement SignUpWithEmailUseCase with user creation

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/auth/domain/SignUpWithEmailUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/auth/domain/SignUpWithEmailUseCase.kt
@@ -2,15 +2,28 @@ package com.nextnonce.app.auth.domain
 
 import com.nextnonce.app.core.domain.Result
 import com.nextnonce.app.auth.domain.model.AuthUserModel
+import com.nextnonce.app.auth.domain.model.toUserModel
 import com.nextnonce.app.core.domain.DataError
+import com.nextnonce.app.logging.AppLogger
+import com.nextnonce.app.user.domain.CreateUserUseCase
 
 class SignUpWithEmailUseCase(
-    private val repo: AuthRepository
+    private val repo: AuthRepository,
+    private val createUserUseCase: CreateUserUseCase
 ) {
 
     suspend fun execute(email: String, password: String): Result<AuthUserModel, DataError> {
         return when (val authUser = repo.signUpWithEmail(email, password)) {
-            is Result.Success -> authUser // should be called checking of new user and creating user
+            is Result.Success -> {
+                when ( val user = createUserUseCase.execute(authUser.data.toUserModel())) {
+                    is Result.Success -> authUser // User creation was successful, return authUser
+                    is Result.Error -> {
+                        // If user creation failed, we can log the error and return it
+                        AppLogger.e { "Failed to create user after sign up: ${user.error}" }
+                        Result.Error(user.error)
+                    }
+                }
+            }
             is Result.Error -> Result.Error(authUser.error)
         }
     }
