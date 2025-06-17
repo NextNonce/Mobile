1c80e180398f76c88e2dabcc41524c7283c2e576
bombonchik <sashayuriychuk672@gmail.com>
2025-06-17 16:25:18 +0200

    Add StartViewModel

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/start/presentation/StartViewModel.kt
@@ -0,0 +1,32 @@
+package com.nextnonce.app.start.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nextnonce.app.auth.domain.IsUserSignedInUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class StartViewModel (
+    private val isUserSignedInUseCase: IsUserSignedInUseCase
+) : ViewModel() {
+    private val _state = MutableStateFlow(StartState())
+    val state = _state
+        .onStart {
+            checkUserSignedIn()
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = StartState()
+        )
+
+    private suspend fun checkUserSignedIn() {
+        val isSignedIn = isUserSignedInUseCase.execute()
+        if (isSignedIn) {
+            _state.value = _state.value.copy(isUserSignedIn = true, isLoading = false)
+        } else {
+            _state.value = _state.value.copy(isUserSignedIn = false, isLoading = false)
+        }
+    }
+}
