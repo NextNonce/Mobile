367c2253959d5ab34599f8228b99b8c68aba2ac3
bombonchik <sashayuriychuk672@gmail.com>
2025-06-18 00:15:42 +0200

    Properly implement SignInWithGoogleUseCase with user creation

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/auth/domain/SignInWithGoogleUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/auth/domain/SignInWithGoogleUseCase.kt
@@ -2,24 +2,41 @@ package com.nextnonce.app.auth.domain
 
 import com.nextnonce.app.core.domain.Result
 import com.nextnonce.app.auth.domain.model.AuthUserModel
+import com.nextnonce.app.auth.domain.model.toUserModel
 import com.nextnonce.app.core.domain.DataError
 import com.nextnonce.app.core.platform.Platform
 import com.nextnonce.app.core.platform.currentPlatform
+import com.nextnonce.app.logging.AppLogger
+import com.nextnonce.app.user.domain.CreateUserUseCase
 
 class SignInWithGoogleUseCase(
     private val repo: AuthRepository,
+    private val createUserUseCase: CreateUserUseCase
 ) {
 
     suspend fun execute(): Result<AuthUserModel, DataError> {
-        return if (currentPlatform is Platform.Android) {
-            when (val authUser = repo.signInWithGoogle()) {
-                is Result.Success -> authUser // should be called checking of new user and creating user
-                is Result.Error -> Result.Error(authUser.error)
-            }
-        } else {
-            Result.Error(
+        if (currentPlatform !is Platform.Android) {
+            AppLogger.e { "SignInWithGoogleUseCase can only be executed on Android platform." }
+            return Result.Error(
                 DataError.Remote.UNKNOWN
             )
+        }
+        return when (val authUser = repo.signInWithGoogle()) {
+            is Result.Success -> {
+                val isNewUser = repo.isNewAuthUser() ?: true
+                if (isNewUser) {
+                    // If it's a new user, we can create a user model from the auth user
+                    when ( val user = createUserUseCase.execute(authUser.data.toUserModel())) {
+                        is Result.Success -> {}  // User creation was successful,
+                        is Result.Error -> {
+                            AppLogger.e { "Failed to create user after sign in with google: ${user.error}" }
+                            Result.Error(user.error)
+                        }
+                    }
+                }
+                authUser
+            }
+            is Result.Error -> Result.Error(authUser.error)
         }
     }
 }
