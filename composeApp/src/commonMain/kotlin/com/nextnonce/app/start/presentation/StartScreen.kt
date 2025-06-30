389628b0ac82372cdbf2188ace176049cc5f604c
bombonchik <sashayuriychuk672@gmail.com>
2025-06-30 08:46:05 +0200

    StartScreen: Put loading to Box

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/start/presentation/StartScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/start/presentation/StartScreen.kt
@@ -1,9 +1,13 @@
 package com.nextnonce.app.start.presentation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import nextnonce.composeapp.generated.resources.Res
 import nextnonce.composeapp.generated.resources.loading
@@ -19,11 +23,15 @@ fun StartScreen(
     val state by startViewModel.state.collectAsStateWithLifecycle()
 
     if (state.isLoading) {
-        Text(
-            text = stringResource(Res.string.loading),
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.primary
-        )
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = stringResource(Res.string.loading),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        }
     } else {
         if (state.isUserSignedIn) {
             onUserSignedIn()
