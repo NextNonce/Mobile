7b2870c8676c0eb5f65dd9648c0a55d92526b14f
bombonchik <sashayuriychuk672@gmail.com>
2025-07-01 23:56:06 +0200

    Portfolio: Add PortfolioRepository

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/portfolio/domain/PortfolioRepository.kt
@@ -0,0 +1,25 @@
+package com.nextnonce.app.portfolio.domain
+
+import com.nextnonce.app.core.domain.Result
+import com.nextnonce.app.core.domain.DataError
+import com.nextnonce.app.core.domain.EmptyResult
+import com.nextnonce.app.portfolio.domain.model.CreatePortfolioWalletCommand
+import com.nextnonce.app.portfolio.domain.model.PortfolioBalancesModel
+import com.nextnonce.app.portfolio.domain.model.PortfolioModel
+import com.nextnonce.app.portfolio.domain.model.PortfolioWalletModel
+import kotlinx.coroutines.flow.Flow
+
+
+interface PortfolioRepository {
+    fun portfoliosFlow(): Flow<Result<List<PortfolioModel>, DataError>>
+    suspend fun createPortfolioWallet(
+        id: String,
+        command: CreatePortfolioWalletCommand
+    ): EmptyResult<DataError>
+
+    fun portfolioWalletsFlow(
+        id: String
+    ): Flow<Result<List<PortfolioWalletModel>, DataError>>
+
+    fun cachedBalancesFlow(id: String): Flow<Result<PortfolioBalancesModel, DataError>>
+}
