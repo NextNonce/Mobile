8646a701d734d77f9e2132967990112159e6144e
bombonchik <sashayuriychuk672@gmail.com>
2025-07-02 09:48:49 +0200

    Portfolio: Add GetPortfolioWalletsUseCase

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/portfolio/domain/GetPortfolioWalletsUseCase.kt
@@ -0,0 +1,14 @@
+package com.nextnonce.app.portfolio.domain
+
+import com.nextnonce.app.core.domain.DataError
+import com.nextnonce.app.core.domain.Result
+import com.nextnonce.app.portfolio.domain.model.PortfolioWalletModel
+import kotlinx.coroutines.flow.Flow
+
+class GetPortfolioWalletsUseCase(
+    private val portfolioRepository: PortfolioRepository,
+) {
+    suspend fun execute(id: String): Flow<Result<List<PortfolioWalletModel>, DataError>> {
+        return portfolioRepository.portfolioWalletsFlow(id)
+    }
+}
