a2ed1c7136ccd14fa8b258f481b5b18b54d6d481
bombonchik <sashayuriychuk672@gmail.com>
2025-07-02 09:49:00 +0200

    Wallet: Add GetWalletAssetBalancesUseCase

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/wallet/domain/GetWalletAssetBalancesUseCase.kt
@@ -0,0 +1,27 @@
+package com.nextnonce.app.wallet.domain
+
+import com.nextnonce.app.core.domain.DataError
+import com.nextnonce.app.core.domain.Result
+import com.nextnonce.app.core.domain.balance.AssetBalance
+import com.nextnonce.app.core.domain.wallet.Wallet
+import com.nextnonce.app.wallet.domain.model.WalletBalancesModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class GetWalletAssetBalancesUseCase(
+    private val walletRepository: WalletRepository
+) {
+
+    fun execute(
+        wallet: Wallet
+    ): Flow<Result<List<AssetBalance>, DataError>> {
+        return walletRepository
+            .balancesFlow(wallet)
+            .map { result: Result<WalletBalancesModel, DataError> ->
+                when (result) {
+                    is Result.Success -> Result.Success(result.data.assetBalances)
+                    is Result.Error -> result
+                }
+            }
+    }
+}
