f652bcbd25f2a4f8e540af37c3228e51b4d605d4
bombonchik <sashayuriychuk672@gmail.com>
2025-07-03 17:47:45 +0200

    JsonHumanReadable: Use AssetDto serializer

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/core/network/JsonConfig.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/core/network/JsonConfig.kt
@@ -1,7 +1,9 @@
 package com.nextnonce.app.core.network
 
 import com.ionspin.kotlin.bignum.serialization.kotlinx.humanReadableSerializerModule
+import com.nextnonce.app.core.data.remote.dto.token.AssetDto
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.plus
 
 /** JSON engine that can handle BigInteger/BigDecimal as decimal strings. */
 val JsonHumanReadable = Json {
@@ -10,5 +12,5 @@ val JsonHumanReadable = Json {
     isLenient         = true
     explicitNulls     = false
 
-    serializersModule = humanReadableSerializerModule
+    serializersModule = humanReadableSerializerModule + AssetDto.module
 }
