0b6ac56a68026c1ad2be12ccf4cbad504c52ba10
bombonchik <sashayuriychuk672@gmail.com>
2025-07-03 13:04:13 +0200

    Enums: Add NumberSign

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/core/enums/NumberSign.kt
@@ -0,0 +1,7 @@
+package com.nextnonce.app.core.enums
+
+enum class NumberSign {
+    NEGATIVE,   // Value is less than zero
+    ZERO,       // Value is exactly zero
+    POSITIVE    // Value is greater than zero
+}
