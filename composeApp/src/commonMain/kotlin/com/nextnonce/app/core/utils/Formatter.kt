7208dd418762af6a56ec947004106354c9fa0ec8
bombonchik <sashayuriychuk672@gmail.com>
2025-07-03 13:02:48 +0200

    Utils: Add formatter function declarations

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/core/utils/Formatter.kt
@@ -0,0 +1,9 @@
+package com.nextnonce.app.core.utils
+
+import com.ionspin.kotlin.bignum.decimal.BigDecimal
+
+expect fun formatQuote(amount: BigDecimal, showDecimal: Boolean = true): String
+
+expect fun formatBalanceNative(amount: BigDecimal, symbol: String): String
+
+expect fun formatPercentage(amount: BigDecimal): String
