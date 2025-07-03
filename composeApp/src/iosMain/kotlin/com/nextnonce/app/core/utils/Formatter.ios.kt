da08dd695cbd9055f68e43775259ab2564f2ca80
bombonchik <sashayuriychuk672@gmail.com>
2025-07-03 13:03:17 +0200

    Utils: Implement formatter functions for iOS

---
+++ composeApp/src/iosMain/kotlin/com/nextnonce/app/core/utils/Formatter.ios.kt
@@ -0,0 +1,62 @@
+package com.nextnonce.app.core.utils
+
+import com.ionspin.kotlin.bignum.decimal.BigDecimal
+import platform.Foundation.NSDecimalNumber
+import platform.Foundation.NSNumberFormatter
+import platform.Foundation.NSNumberFormatterDecimalStyle
+
+actual fun formatQuote(amount: BigDecimal, showDecimal: Boolean): String {
+    val formatter = NSNumberFormatter().apply {
+        numberStyle = NSNumberFormatterDecimalStyle
+
+        when {
+            !showDecimal -> {
+                minimumFractionDigits = 0uL
+                maximumFractionDigits = 0uL
+            }
+            amount >= 0.01 || amount.compareTo(BigDecimal.ZERO) == 0  -> {
+                minimumFractionDigits = 2uL
+                maximumFractionDigits = 2uL
+            }
+            else -> {
+                minimumFractionDigits = 8uL
+                maximumFractionDigits = 8uL
+            }
+        }
+    }
+
+    val decimalNumber = NSDecimalNumber(string = amount.toString())
+    return formatter.stringFromNumber(decimalNumber)?.let { "$$it" } ?: ""
+}
+
+actual fun formatBalanceNative(amount: BigDecimal, symbol: String): String {
+    val formatter = NSNumberFormatter().apply {
+        numberStyle = NSNumberFormatterDecimalStyle
+        when {
+            amount >= 10 || amount.compareTo(BigDecimal.ZERO) == 0  -> {
+                minimumFractionDigits = 3uL
+                maximumFractionDigits = 3uL
+            }
+            else -> {
+                minimumFractionDigits = 8uL
+                maximumFractionDigits = 8uL
+            }
+        }
+    }
+    val decimalNumber = NSDecimalNumber(string = amount.toString())
+    val formattedAmount = formatter.stringFromNumber(decimalNumber) ?: ""
+
+    return "$formattedAmount $symbol"
+}
+
+actual fun formatPercentage(amount: BigDecimal): String {
+    val formatter = NSNumberFormatter().apply {
+        numberStyle = NSNumberFormatterDecimalStyle
+        minimumFractionDigits = 2uL
+        maximumFractionDigits = 2uL
+    }
+    val decimalNumber = NSDecimalNumber(double = amount.doubleValue(exactRequired = false))
+    val prefix = if (amount > BigDecimal.ZERO) "+" else ""
+    val formattedAmount = formatter.stringFromNumber(decimalNumber) ?: ""
+    return "$prefix$formattedAmount%"
+}
