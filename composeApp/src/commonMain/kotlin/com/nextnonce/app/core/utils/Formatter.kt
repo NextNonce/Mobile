7ae065fe53002d722c6de6ce98b57b229471ddfa
bombonchik <sashayuriychuk672@gmail.com>
2025-07-04 23:34:51 +0200

    Formatter: Add formatQuoteChange

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/core/utils/Formatter.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/core/utils/Formatter.kt
@@ -4,6 +4,8 @@ import com.ionspin.kotlin.bignum.decimal.BigDecimal
 
 expect fun formatQuote(amount: BigDecimal, showDecimal: Boolean = true): String
 
+expect fun formatQuoteChange(amount: BigDecimal, showDecimal: Boolean = true): String
+
 expect fun formatBalanceNative(amount: BigDecimal, symbol: String): String
 
 expect fun formatPercentage(amount: BigDecimal): String
