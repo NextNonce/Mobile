e2399be1705c383db57f867248b6030277eddbd8
bombonchik <sashayuriychuk672@gmail.com>
2025-07-04 23:28:31 +0200

    WalletDatabase: Add di for iOS

--- composeApp/src/iosMain/kotlin/com/nextnonce/app/di/Module.ios.kt
+++ composeApp/src/iosMain/kotlin/com/nextnonce/app/di/Module.ios.kt
@@ -1,7 +1,12 @@
 package com.nextnonce.app.di
 
+import androidx.room.RoomDatabase
+import com.nextnonce.app.core.database.wallet.WalletDatabase
+import com.nextnonce.app.core.database.wallet.getWalletDatabaseBuilder
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val platformModule = module {
@@ -9,4 +14,7 @@ actual val platformModule = module {
     single<HttpClientEngine> {
         Darwin.create()
     }
+
+    // database
+    singleOf(::getWalletDatabaseBuilder).bind<RoomDatabase.Builder<WalletDatabase>>()
 }
