640ad04aa459dd0ada8c754c8c95a18cc088c95f
bombonchik <sashayuriychuk672@gmail.com>
2025-07-04 23:47:37 +0200

    StartScreen: Use LoadingOverlay

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/start/presentation/StartScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/start/presentation/StartScreen.kt
@@ -1,17 +1,9 @@
 package com.nextnonce.app.start.presentation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import nextnonce.composeapp.generated.resources.Res
-import nextnonce.composeapp.generated.resources.loading
-import org.jetbrains.compose.resources.stringResource
+import com.nextnonce.app.core.presentation.LoadingOverlay
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -23,15 +15,7 @@ fun StartScreen(
     val state by startViewModel.state.collectAsStateWithLifecycle()
 
     if (state.isLoading) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                text = stringResource(Res.string.loading),
-                style = MaterialTheme.typography.headlineSmall,
-            )
-        }
+        LoadingOverlay()
     } else {
         if (state.isUserSignedIn) {
             onUserSignedIn()
