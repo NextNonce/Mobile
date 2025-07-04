62febfb9ff7813bdfe36ae9f74dc1007f8ca21f9
bombonchik <sashayuriychuk672@gmail.com>
2025-07-04 21:53:24 +0200

    iOS: Lock to portrait orientation

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,9 +2,21 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        // lock *only* upright portrait
+        return .portrait
     }
 }
