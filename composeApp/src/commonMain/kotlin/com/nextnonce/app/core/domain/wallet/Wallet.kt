a1637287318211a40907c9ef84f9eb8312b02f3b
bombonchik <sashayuriychuk672@gmail.com>
2025-07-04 23:33:19 +0200

    Wallet: toWallet() for String

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/core/domain/wallet/Wallet.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/core/domain/wallet/Wallet.kt
@@ -8,3 +8,14 @@ data class Wallet(
 ) {
     val id get() = "$chainType:$address"
 }
+
+fun String.toWallet(): Wallet? {
+    val parts = this.split(":")
+    return if (parts.size == 2) {
+        val chainType = ChainType.valueOf(parts[0])
+        val address = parts[1]
+        Wallet(address, chainType)
+    } else {
+        null
+    }
+}
