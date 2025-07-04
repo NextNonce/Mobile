8423862c15a8648bcd23e4236e4f43b842084ad6
bombonchik <sashayuriychuk672@gmail.com>
2025-07-04 21:56:21 +0200

    RemoteWalletDataSource: Add get WalletDto

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/wallet/data/remote/RemoteWalletDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/wallet/data/remote/RemoteWalletDataSource.kt
@@ -3,7 +3,9 @@ package com.nextnonce.app.wallet.data.remote
 import com.nextnonce.app.core.domain.DataError
 import com.nextnonce.app.core.domain.Result
 import com.nextnonce.app.wallet.data.remote.dto.WalletBalancesDto
+import com.nextnonce.app.wallet.data.remote.dto.WalletDto
 
 interface RemoteWalletDataSource {
+    suspend fun get(address: String): Result<WalletDto, DataError.Remote>
     suspend fun getBalances(address: String): Result<WalletBalancesDto, DataError.Remote>
 }
