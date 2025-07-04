92afeafef734791b817222381c365539bcb28954
bombonchik <sashayuriychuk672@gmail.com>
2025-07-04 23:29:11 +0200

    Wallet: Add WalletDatabase

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/core/database/wallet/WalletDatabase.kt
@@ -0,0 +1,18 @@
+package com.nextnonce.app.core.database.wallet
+
+import androidx.room.ConstructedBy
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.nextnonce.app.core.utils.BigDecimalConverters
+import com.nextnonce.app.wallet.data.local.WalletBalancesCacheDao
+import com.nextnonce.app.wallet.data.local.WalletBalancesCacheEntity
+
+@Database(entities = [
+    WalletBalancesCacheEntity::class],
+    version = 1)
+@TypeConverters(BigDecimalConverters::class)
+@ConstructedBy(WalletDatabaseCreator::class)
+abstract class WalletDatabase : RoomDatabase() {
+    abstract fun walletBalancesCacheDao(): WalletBalancesCacheDao
+}
