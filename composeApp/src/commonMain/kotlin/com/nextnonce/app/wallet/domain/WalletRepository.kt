a75bee0b9752bac5a5f3369362674b0ad511d70f
bombonchik <sashayuriychuk672@gmail.com>
2025-07-04 21:58:12 +0200

    WalletRepository: Add getWalletById

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/wallet/domain/WalletRepository.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/wallet/domain/WalletRepository.kt
@@ -3,9 +3,15 @@ package com.nextnonce.app.wallet.domain
 import com.nextnonce.app.core.domain.Result
 import com.nextnonce.app.core.domain.DataError
 import com.nextnonce.app.wallet.domain.model.WalletBalancesModel
+import com.nextnonce.app.wallet.domain.model.WalletModel
 import kotlinx.coroutines.flow.Flow
 
 interface WalletRepository {
+
+    suspend fun getWalletById(
+        walletId: String
+    ): Result<WalletModel, DataError>
+
     fun balancesFlow(
         walletId: String
     ): Flow<Result<WalletBalancesModel, DataError>>
