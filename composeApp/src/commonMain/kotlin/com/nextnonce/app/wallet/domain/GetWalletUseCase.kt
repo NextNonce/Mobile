bf26565e155ddf81bb6616a3e7442024b41e5558
bombonchik <sashayuriychuk672@gmail.com>
2025-07-04 23:50:41 +0200

    Wallet: Add GetWalletUseCase

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/wallet/domain/GetWalletUseCase.kt
@@ -0,0 +1,15 @@
+package com.nextnonce.app.wallet.domain
+
+import com.nextnonce.app.core.domain.Result
+import com.nextnonce.app.core.domain.DataError
+import com.nextnonce.app.wallet.domain.model.WalletModel
+
+class GetWalletUseCase(
+    private val walletRepository: WalletRepository
+) {
+    suspend fun execute(
+        walletId: String
+    ): Result<WalletModel, DataError> {
+        return walletRepository.getWalletById(walletId)
+    }
+}
