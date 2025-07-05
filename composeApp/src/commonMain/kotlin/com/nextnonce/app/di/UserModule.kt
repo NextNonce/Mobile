34223aa8a4d5f18c46023577d24aca06548dad94
bombonchik <sashayuriychuk672@gmail.com>
2025-07-05 17:48:33 +0200

    DI: Add UserModule

---
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/di/UserModule.kt
@@ -0,0 +1,19 @@
+package com.nextnonce.app.di
+
+import com.nextnonce.app.user.data.UserRepositoryImpl
+import com.nextnonce.app.user.data.remote.RemoteUserDataSource
+import com.nextnonce.app.user.data.remote.impl.RemoteUserDataSourceImpl
+import com.nextnonce.app.user.domain.CreateUserUseCase
+import com.nextnonce.app.user.domain.GetUserUseCase
+import com.nextnonce.app.user.domain.UserRepository
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+
+val userModule = module {
+    singleOf(::UserRepositoryImpl).bind<UserRepository>()
+    singleOf(::CreateUserUseCase)
+    singleOf(::GetUserUseCase)
+    singleOf(::RemoteUserDataSourceImpl).bind<RemoteUserDataSource>()
+}
