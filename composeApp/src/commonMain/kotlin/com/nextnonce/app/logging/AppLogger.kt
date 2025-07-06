27c6e7f006258521f86db62e3bd75878fcc52e5b
bombonchik <sashayuriychuk672@gmail.com>
2025-07-06 23:36:06 +0200

    Logging: Add explanatory comments

--- composeApp/src/commonMain/kotlin/com/nextnonce/app/logging/AppLogger.kt
+++ composeApp/src/commonMain/kotlin/com/nextnonce/app/logging/AppLogger.kt
@@ -3,6 +3,12 @@ package com.nextnonce.app.logging
 import co.touchlab.kermit.Logger
 import co.touchlab.kermit.StaticConfig
 
+/**
+ * AppLogger is a singleton instance of Logger configured for the application.
+ * It uses a static configuration and is tagged with "App".
+ *
+ * Use [appLogger] function to create a logger with a specific feature tag.
+ */
 object AppLogger : Logger(
     config = StaticConfig(),
     tag = "App",
